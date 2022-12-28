cd679bf8ba277fe701af43d35ebec07d6e97f3b0
korpaxdev <korpaxdev@gmail.com>
2022-12-28 14:31:42 +0300

    set limit=20 in url

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,3 @@
 HOST = 'https://hh.ru'
-MAIN_URL = HOST + '/search/vacancy?text=python&area=1&area=2'
+MAIN_URL = HOST + '/search/vacancy?text=python&area=1&area=2&limit=20'
 KEYWORDS = ['Django', 'Flask']
